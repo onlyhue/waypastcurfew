54a9a2e3281ab9267bba7ecd51491001d95f5885
onlyhuefyp <is480.onlyHUE@gmail.com>
2015-06-28 16:25:49 +0800

    S1
    Login/Logout
    Connect via social media
    CRUD of accounts

---
+++ www/js/app.js
@@ -0,0 +1,53 @@
+var app = angular.module("starter", ["ionic", "firebase", "ngCordova"])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+  $stateProvider
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'loginController'
+  })
+
+  .state('main', {
+    url: '/main',
+    templateUrl: 'templates/main.html',
+    controller: 'mainController'
+  })
+
+  .state('items', {
+    url: '/items',
+    templateUrl: 'templates/items.html',
+    controller: 'itemsController'
+  })
+
+  .state('profile', {
+    url: '/profile',
+    templateUrl: 'templates/profile.html',
+    controller: 'profileController'
+  })
+
+  .state('create', {
+    url: '/create',
+    templateUrl: 'templates/create.html',
+    controller: 'createController'
+  })
+
+
+  $ionicConfigProvider.views.forwardCache(true);
+
+  $urlRouterProvider.otherwise('/login');
+});
---
+++ www/js/factories/songsFactory.js
@@ -0,0 +1,18 @@
+app.factory("songsFactory", function() {
+    var songs = [
+        {
+            url: "https://p.scdn.co/mp3-preview/5cd95b47674498eab79c126ccdfebcc63fd2cfe1",
+            media: {}
+        },
+        {
+            url: "https://p.scdn.co/mp3-preview/8f7b6745580f5264b3ab679e2661ddfb8a333714",
+            media: {}
+        },
+        {
+            url: "https://p.scdn.co/mp3-preview/c565ed35ee000fa18bad6b6180965e3eb165cfb2",
+            media: {}
+        }
+    ];
+
+    return songs;
+});
