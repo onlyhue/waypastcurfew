2b37e6a62fd6bc9a6c8912f8435faa54c800a57c
onlyhuefyp <is480.onlyHUE@gmail.com>
2015-07-02 11:50:59 +0800

    S2 | Amendments to Social Media Connect / Log-in
    Changes to address Issue #1

---
+++ platforms/ios/www/js/controllers/createController.js
@@ -0,0 +1,50 @@
+app.controller("createController", function($scope, usersFactory, $firebaseAuth, $state) {
+    // retrieve firebase reference + authentication object
+    var ref = usersFactory.getRef();
+    var authObj = usersFactory.getAuthObj();
+
+    // on page enter, prepare page based on facebook or email account
+    $scope.$on('$ionicView.beforeEnter', function() {
+        $scope.data = usersFactory.getProfile();
+        if ($scope.data.password == null) {
+            // facebook account, capitalize first letter of gender
+            $scope.data.gender = $scope.data.gender.charAt(0).toUpperCase() + $scope.data.gender.slice(1);
+        } else {
+            // email account, hide email and password fields
+            $scope.hideEmail = true;
+            $scope.hidePassword = true;
+        }
+    })
+
+    // create account method
+    $scope.create = function() {
+        authObj.$createUser({
+            email: $scope.data.email,
+            password: $scope.data.password
+        }).then(function (userData) {
+            //******account created successfully, update user profile
+            //*****usersFactory.setProfile($scope.data.email, $scope.data.first_name, $scope.data.last_name, $scope.data.gender, "", $scope.data.password);
+            // login to firebase
+            return authObj.$authWithPassword({
+                email: $scope.data.email,
+                password: $scope.data.password
+            });
+        }).then(function (authData) {
+            // account successfully logged in, set up profile object for firebase
+            var profile = {
+                email: $scope.data.email,
+                first_name: $scope.data.first_name,
+                last_name: $scope.data.last_name,
+                gender: $scope.data.gender,
+                picture: $scope.data.picture
+            };
+            // set profile data in firebase
+            ref.child($scope.data.email.replace(/\./g, '')).set(profile);
+            // go to main page
+            $state.go("main");
+        }).catch(function (error) {
+            // error (most likely invalid password), display error message
+            $scope.error = "Invalid password!";
+        })
+    };
+});
